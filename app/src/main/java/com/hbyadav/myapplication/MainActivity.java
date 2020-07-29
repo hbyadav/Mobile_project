b0a7caefc4ecb504cf9b50f2229694cb8a0fadf3
HBYWQB <hbywqb@mail.umkc.edu>
2020-07-28 22:25:31 -0500

    added news API

--- app/src/main/java/com/hbyadav/myapplication/MainActivity.java
+++ app/src/main/java/com/hbyadav/myapplication/MainActivity.java
@@ -5,13 +5,39 @@ import android.os.Bundle;
 import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
 import com.android.tourguide.R;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity{
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    ArrayList<String> rssLinks = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle(R.string.Student_Home);
+
+	    Button btnRediff = findViewById(R.id.btnRediff);
+        Button btnCinemaBlend = findViewById(R.id.btnCinemaBlend);
+        btnRediff.setOnClickListener(this);
+        btnCinemaBlend.setOnClickListener(this);
+
+        rssLinks.add("http://www.rediff.com/rss/moviesreviewsrss.xml");
+        rssLinks.add("http://www.cinemablend.com/rss_review.php");
+    }
+
+	@Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnRediff:
+                startActivity(new Intent(MainActivity.this, RSSFeedActivity.class).putExtra("rssLink", rssLinks.get(0)));
+                break;
+
+            case R.id.btnCinemaBlend:
+                startActivity(new Intent(MainActivity.this, RSSFeedActivity.class).putExtra("rssLink", rssLinks.get(1)));
+                break;
+        }
     }
 
     // Sign up for the app
